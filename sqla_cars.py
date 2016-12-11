6cd07e4d8244a0962a161b0e51d8a18f4ac47192
MWILA AARON SIMPITO <amsimpito@gmail.com>
2016-12-11 02:01:21 +0200

    car.db added

---
+++ sqla_cars.py
@@ -0,0 +1,16 @@
+#import the sqlite3 library
+import sqlite3
+
+#create database if database doesn't already exist
+conn = sqlite3.connect("cars.db")
+
+#get a  cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+#create a table
+cursor.execute(""" CREATE TABLE inventory
+(Make TEXT, Model TEXT, Quantity INT)
+""")
+
+#close the database connection
+conn.close()
